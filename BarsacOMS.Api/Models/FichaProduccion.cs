1e14f38ee6752d1dd83f45a572bf08045b9686b1
Luci Barbero <lucibarbero173@gmail.com>
2026-08-04 12:50:12 -0300

    aa

--- BarsacOMS.Api/Models/FichaProduccion.cs
+++ BarsacOMS.Api/Models/FichaProduccion.cs
@@ -1,13 +1,18 @@
-﻿namespace BarsacOMS.Api.Models
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation; // <--- Importante
+
+namespace BarsacOMS.Api.Models
 {
     public class FichaProduccion
     {
         public int Id { get; set; }
 
         public int OrdenId { get; set; }
+
+        [ValidateNever] // <--- AGREGÁ ESTO
         public OrdenTrabajo Orden { get; set; }
 
-        public string Modista { get; set; } // Ale, Tere, Vero, Loli, Beti, Sarita, Vicki
+        public string Modista { get; set; }
 
         public List<DetalleFichaProduccion> Items { get; set; } = new();
     }
